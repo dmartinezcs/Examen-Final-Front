221da7be0993578618f4a5e40b17e0992b40ff54
dmartinezcs <dmartinezc4@alumnos.nebrija.es>
2026-05-25 12:29:51 +0000

    Extra api Calls

--- examen-final/src/lib/api/rick.ts
+++ examen-final/src/lib/api/rick.ts
@@ -14,3 +14,29 @@ export const getCharacterById = async (id: number) => {
     const response = await api.get<CharacterT>(`/character/${id}`);
     return response.data;
 };
+
+export const getCharactersByGender = async(gender: string): Promise<ResultCharactersT> => {
+    const response = await api.get(`/character/?gender=${ gender }`);
+    return response.data;
+};
+
+export const getCharactersByStatus = async(status: string): Promise<ResultCharactersT> => {
+    const response = await api.get(`/character/?status=${ status }`);
+    return response.data;
+};
+
+export const getCharactersByNames = async(name: string): Promise<ResultCharactersT> => {
+    const response = await api.get(`/character/?name=${ name }`);
+    return response.data;
+};
+
+export const getFilteredCharacters = async (filters: { gender?: string; status?: string; name?: string }): Promise<ResultCharactersT> => {
+    const { gender, status, name } = filters;
+    let url = `https://rickandmortyapi.com/api/character/?`;
+    if (name) url += `name=${name}&`;
+    if (gender) url += `gender=${gender}&`;
+    if (status) url += `status=${status}`;
+    
+    const response = await api.get(url);
+    return response.data;
+};
